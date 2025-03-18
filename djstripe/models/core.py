c344c5c216f6dbf7e4a54750c1cf064ce1185145
guilleRFLY <guille.rightfully@gmail.com>
2025-03-18 12:36:53 -0300

    Update core.py fix length from 19 to 21

--- djstripe/models/core.py
+++ djstripe/models/core.py
@@ -2151,7 +2151,7 @@ class Price(StripeModel):
     unit_amount_decimal = StripeDecimalCurrencyAmountField(
         null=True,
         blank=True,
-        max_digits=19,
+        max_digits=21,
         decimal_places=12,
         help_text=(
             "The unit amount in cents to be charged, represented as a decimal "
